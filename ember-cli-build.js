27225d0c5de7df2fe994faf9d630494dd4508efb
Marc Brakken <marc.brakken@gmail.com>
2016-04-30 19:58:24 -0500

    no ember data, add purecss

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,6 +4,9 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
+    pure: {
+      modules: ['grids', 'grids-responsive', 'menus', 'forms']
+    }
     // Add options here
   });
 
